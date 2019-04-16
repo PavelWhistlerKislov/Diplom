469aeb13b593f91fc4061f61f638b06ef2803b32
PavelWhistlerKislov <thechasercloud@mail.ru>
2019-04-16 20:43:35 +0300

    Продолжаю мучать регистрацию

--- src/main/java/root/domain/config/MvcConfig.java
+++ src/main/java/root/domain/config/MvcConfig.java
@@ -1,6 +1,5 @@
 package root.domain.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- src/main/java/root/domain/controllers/MainController.java
+++ src/main/java/root/domain/controllers/MainController.java
@@ -19,4 +19,6 @@ public class MainController {
     public String main() {
         return "main";
     }
+
+
 }
